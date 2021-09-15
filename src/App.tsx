bc67a71030440fa07e02b4746d38a4bc6b5ab027
Felipe <fellipefreiire3@gmail.com>
2021-09-15 17:34:36 -0300

    react-modal config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { createServer } from "miragejs";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 createServer({
   routes() {
@@ -23,10 +27,26 @@ createServer({
 });
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
